fe42a48823a76154805c163cf7b4413ab0235e50
Prashant Garde <prashant.garde@FISGLOBAL.COM>
2021-12-26 17:31:16 +0530

    Error Page connected

--- Startup.cs
+++ Startup.cs
@@ -21,21 +21,14 @@ namespace ExploreCalifornia
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseExceptionHandler("/error.html");
 
-            app.UseFileServer();
-            //app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
+            //if (env.IsDevelopment())
             //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseFileServer();
+
         }
     }
 }
